65e5b83efed9332bbf7ad70aebb456abb98e897e
Moshiur Rahman <rmoshiur705@gmail.com>
2023-12-20 08:11:37 +0600

    Added docs to show suggestion and attached event handlers

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,6 +2,29 @@ import {forwardRef} from 'react';
 import PropTypes from "prop-types";
 import style from "./Button.module.css";
 
+
+
+/**
+ * @typedef {object} ButtonProps
+ * @property {string} [title='Easy Button']
+ * @property {string} [height='60px']
+ * @property {boolean} [fullWidth=false]
+ * @property {boolean} [disabled=false]
+ * @property {string} color
+ * @property {React.ReactNode} prefixIcon
+ * @property {React.ReactNode} suffixIcon
+ * @property {'primary' | 'danger' | 'success' | 'warning'} type - One of ['primary', 'danger', 'success', 'warning']
+ * @property {string} [effect='ripple']
+ * @property {() => void} onClick - Click event handler
+ * @property {() => void} onMouseEnter - Mouse enter event handler
+ * @property {() => void} onMouseLeave - Mouse leave event handler
+ * @property {() => void} onFocus - Focus event handler
+ * @property {() => void} onBlur - Blur event handler
+ */
+
+/**
+ * @param {ButtonProps} props
+ */
 function Button(props) {
 
     const {
@@ -14,12 +37,24 @@ function Button(props) {
         prefixIcon,
         suffixIcon,
         type,
-        effect = 'ripple'
+        effect = 'ripple',
+        onClick,
+        onMouseEnter,
+        onMouseLeave,
+        onFocus,
+        onBlur
     } = props;
-    console.log(props);
+    console.log(props, children);
 
     return (
-        <button className={style['ei-btn']} disabled={disabled}>{children ? children : title}</button>
+        <button className={`${style['ei-btn']} ${style[type]}`} 
+            disabled={disabled}
+            onClick={onClick}
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+            onFocus={onFocus}
+            onBlur={onBlur}
+            >{children ? children : title}</button>
     )
 }
 
